0df27d947319b04bc39fea61d431974f03665499
TaeHwan-Gwak <taehw0617@cau.ac.kr>
2023-11-13 14:28:21 +0900

    feat : image_picker from phone

--- lib/example.dart
+++ lib/example.dart
@@ -1,48 +1,90 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:kakao_map_plugin/kakao_map_plugin.dart';
+import 'dart:io';
 
-void main() async{
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+
+void main() {
   WidgetsFlutterBinding.ensureInitialized();
 
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  XFile? _image; //이미지를 담을 변수 선언
+  final ImagePicker picker = ImagePicker(); //ImagePicker 초기화
+
+  //이미지를 가져오는 함수
+  Future getImage(ImageSource imageSource) async {
+    //pickedFile에 ImagePicker로 가져온 이미지가 담긴다.
+    final XFile? pickedFile = await picker.pickImage(source: imageSource);
+    if (pickedFile != null) {
+      setState(() {
+        _image = XFile(pickedFile.path); //가져온 이미지를 _image에 저장
+        File _file = File(pickedFile.path);
+        FirebaseStorage.instance
+            .ref("test/test_image")
+            .putFile(_file);
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'PHOTOIS',
-      theme: ThemeData(
-        primarySwatch: Colors.blueGrey,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('PHOTOIS'),
+        ),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SizedBox(height: 30, width: double.infinity),
+            _buildPhotoArea(),
+            SizedBox(height: 20),
+            _buildButton(),
+          ],
+        ),
       ),
-      home: const MyHomePage(),
     );
   }
-}
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key}) : super(key: key);
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  void initState() {
-    super.initState();
+  Widget _buildPhotoArea() {
+    return _image != null
+        ? Container(
+      width: 300,
+      height: 300,
+      child: Image.file(File(_image!.path)), //가져온 이미지를 화면에 띄워주는 코드
+    )
+        : Container(
+      width: 300,
+      height: 300,
+      color: Colors.grey,
+    );
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('PHOTOIS'),
-      ),
-      // body: ,
+  Widget _buildButton() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        SizedBox(width: 30),
+        ElevatedButton(
+          onPressed: () {
+            getImage(ImageSource.gallery); //getImage 함수를 호출해서 갤러리에서 사진 가져오기
+          },
+          child: Text("갤러리"),
+        ),
+      ],
     );
   }
 }
