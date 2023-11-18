c51350c0dd46dcfd4ebf068d4ad2983c2060cb61
heji0826 <heji0826@naver.com>
2023-11-18 14:17:05 +0900

    feat: google sign in

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,16 @@
-import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:photois/firebase_options.dart';
-import 'package:google_sign_in/google_sign_in.dart';
-import 'package:photois/routes.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:intl/date_symbol_data_local.dart';
+import 'package:photois/firebase_options.dart';
+import 'package:photois/routes.dart';
+import 'package:photois/service/firebase.auth.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await FbAuth.init();
+
   initializeDateFormatting().then((_) => runApp(const MyApp()));
 }
 
@@ -18,9 +20,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-        title: 'PHOTOIS',
-        theme: ThemeData(primarySwatch: Colors.grey, fontFamily: 'BMHANNAPro'),
-        initialRoute: '/',
-        getPages: page);
+      title: 'PHOTOIS',
+      theme: ThemeData(primarySwatch: Colors.grey, fontFamily: 'BMHANNAPro'),
+      initialRoute: '/',
+      getPages: page,
+      debugShowCheckedModeBanner: false,
+    );
   }
 }
