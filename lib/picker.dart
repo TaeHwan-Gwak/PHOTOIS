990dd40145a000f427a4cd39eb4be3dfd7bc847a
TaeHwan-Gwak <taehw0617@cau.ac.kr>
2023-11-18 21:48:13 +0900

    docs : picker.dart

--- lib/picker.dart
+++ lib/picker.dart
@@ -3,28 +3,29 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:intl/intl.dart';
 import 'package:path/path.dart' as p;
 import 'package:path_provider/path_provider.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_storage/firebase_storage.dart';
-import 'package:permission_handler/permission_handler.dart';
+
 import 'package:native_exif/native_exif.dart';
 
 void main() {
-  WidgetsFlutterBinding.ensureInitialized();
-  runApp(const MyApp());
+  runApp(const MaterialApp(home: MyApp()));
 }
 
 class MyApp extends StatefulWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({
+    Key? key,
+  }) : super(key: key);
 
   @override
   State<MyApp> createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
-  XFile? _image;
-  final ImagePicker picker = ImagePicker();
+  final picker = ImagePicker();
+
+  XFile? pickedFile;
   Exif? exif;
   Map<String, Object>? attributes;
   DateTime? shootingDate;
@@ -63,24 +64,18 @@ class _MyAppState extends State<MyApp> {
     );
   }
 
-  Future getImage(ImageSource imageSource) async {
-    final XFile? pickedFile = await picker.pickImage(source: imageSource);
-    if (pickedFile != null) {
-      File _file = File(pickedFile.path);
-
-      // Firebase Storage에 이미지 업로드
-      await FirebaseStorage.instance.ref("test/test_image.jpeg").putFile(_file);
-
-      // Exif 데이터 가져오기
-      exif = await Exif.fromPath(pickedFile.path);
-      attributes = await exif!.getAttributes();
-      shootingDate = await exif!.getOriginalDate();
-      coordinates = await exif!.getLatLong();
-
-      setState(() {
-        _image = XFile(pickedFile.path);
-      });
+  Future getImage() async {
+    pickedFile = await picker.pickImage(source: ImageSource.gallery);
+    if (pickedFile == null) {
+      return;
     }
+
+    exif = await Exif.fromPath(pickedFile!.path);
+    attributes = await exif!.getAttributes();
+    shootingDate = await exif!.getOriginalDate();
+    coordinates = await exif!.getLatLong();
+
+    setState(() {});
   }
 
   Future closeImage() async {
@@ -95,50 +90,169 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('PHOTOIS'),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              _image != null
-                  ? Container(
-                width: 300,
-                height: 300,
-                child: Image.file(File(_image!.path)),
-              )
-                  : Container(
-                width: 300,
-                height: 300,
-                color: Colors.grey,
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Plugin example app'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            if (pickedFile == null)
+              const Text("Please open an image.")
+            else
+              Column(
+                children: [
+                  Text("The selected image has ${attributes?.length ?? 0} attributes."),
+                  Text("It was taken at ${shootingDate.toString()}"),
+                  Text(attributes?["UserComment"]?.toString() ?? ''),
+                  Text("Attributes: $attributes"),
+                  Text("Coordinates: $coordinates"),
+                  TextButton(
+                    onPressed: () async {
+                      try {
+                        final dateFormat = DateFormat('yyyy:MM:dd HH:mm:ss');
+                        await exif!.writeAttribute('DateTimeOriginal', dateFormat.format(DateTime.now()));
+
+                        shootingDate = await exif!.getOriginalDate();
+                        attributes = await exif!.getAttributes();
+
+                        setState(() {});
+                      } catch (e) {
+                        showError(e);
+                      }
+                    },
+                    child: const Text('Update date attribute'),
+                  ),
+                  TextButton(
+                    onPressed: () async {
+                      try {
+                        // Get original attributes.
+                        final attrs = await exif!.getAttributes();
+
+                        debugPrint('Original attributes of ${pickedFile!.path}:');
+                        debugPrint(attrs.toString());
+
+                        final dateFormat = DateFormat('yyyy:MM:dd HH:mm:ss');
+                        debugPrint('Write DateTimeOriginal ${dateFormat.format(DateTime.now())}');
+                        await exif!.writeAttribute('DateTimeOriginal', dateFormat.format(DateTime.now()));
+
+                        await exif!.writeAttributes({
+                          'GPSLatitude': '1.0',
+                          'GPSLatitudeRef': 'N',
+                          'GPSLongitude': '2.0',
+                          'GPSLongitudeRef': 'W',
+                        });
+
+                        shootingDate = await exif!.getOriginalDate();
+                        attributes = await exif!.getAttributes();
+
+                        debugPrint('New attributes:');
+                        debugPrint(shootingDate.toString());
+                        debugPrint(attributes.toString());
+
+                        final dir = await getApplicationDocumentsDirectory();
+                        final newPath = p.join(dir.path, p.basename(pickedFile!.path));
+
+                        debugPrint('New path:');
+                        debugPrint(newPath);
+
+                        final newFile = File(newPath);
+                        await newFile.writeAsBytes(await pickedFile!.readAsBytes());
+
+                        pickedFile = XFile(newPath);
+                        exif = await Exif.fromPath(newPath);
+                        attributes = await exif!.getAttributes();
+                        shootingDate = await exif!.getOriginalDate();
+                        coordinates = await exif!.getLatLong();
+
+                        debugPrint('Attributes of $newPath:');
+                        debugPrint(shootingDate.toString());
+                        debugPrint(coordinates.toString());
+                        debugPrint(attributes.toString());
+
+                        setState(() {});
+                      } catch (e) {
+                        showError(e);
+                      }
+                    },
+                    child: const Text('Update, store and reload attributes'),
+                  ),
+                  TextButton(
+                    onPressed: () async {
+                      try {
+                        await exif!.writeAttribute('Orientation', '1');
+
+                        attributes = await exif!.getAttributes();
+
+                        setState(() {});
+                      } catch (e) {
+                        showError(e);
+                      }
+                    },
+                    child: const Text('Set orientation to 1'),
+                  ),
+                  TextButton(
+                    onPressed: () async {
+                      try {
+                        await exif!.writeAttributes({
+                          'GPSLatitude': '1.0',
+                          'GPSLatitudeRef': 'N',
+                          'GPSLongitude': '2.0',
+                          'GPSLongitudeRef': 'W',
+                        });
+
+                        shootingDate = await exif!.getOriginalDate();
+                        attributes = await exif!.getAttributes();
+                        coordinates = await exif!.getLatLong();
+
+                        setState(() {});
+                      } catch (e) {
+                        showError(e);
+                      }
+                    },
+                    child: const Text('Update GPS attributes'),
+                  ),
+                  ElevatedButton(
+                    onPressed: closeImage,
+                    style: ButtonStyle(backgroundColor: MaterialStateProperty.all(Colors.red)),
+                    child: const Text('Close image'),
+                  )
+                ],
               ),
-              const SizedBox(height: 20),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: getImage,
+              child: const Text('Open image'),
+            ),
+            if (pickedFile != null)
               ElevatedButton(
-                onPressed: () {
-                  getImage(ImageSource.gallery);
+                onPressed: () async {
+                  try {
+                    final file = File(p.join(Directory.systemTemp.path, 'tempimage.jpg'));
+                    final imageBytes = await pickedFile!.readAsBytes();
+                    await file.create();
+                    await file.writeAsBytes(imageBytes);
+                    final _attributes = await exif?.getAttributes() ?? {};
+                    final newExif = await Exif.fromPath(file.path);
+
+                    _attributes['DateTimeOriginal'] = '2021:05:15 13:00:00';
+                    _attributes['UserComment'] = "This file is user generated!";
+
+                    await newExif.writeAttributes(_attributes);
+
+                    shootingDate = await newExif.getOriginalDate();
+                    attributes = await newExif.getAttributes();
+                    coordinates = await newExif.getLatLong();
+
+                    setState(() {});
+                  } catch (e) {
+                    showError(e);
+                  }
                 },
-                child: const Text('갤러리'),
+                child: const Text("Create file and write exif data"),
               ),
-              if (_image != null)
-                Column(
-                  children: [
-                    Text("The selected image has ${attributes?.length ?? 0} attributes."),
-                    Text("It was taken at ${shootingDate.toString()}"),
-                    Text(attributes?["UserComment"]?.toString() ?? ''),
-                    Text("Attributes: $attributes"),
-                    Text("Coordinates: $coordinates"),
-                    ElevatedButton(
-                      onPressed: closeImage,
-                      style: ButtonStyle(backgroundColor: MaterialStateProperty.all(Colors.red)),
-                      child: const Text('이미지 닫기'),
-                    )
-                  ],
-                ),
-            ],
-          ),
+          ],
         ),
       ),
     );
