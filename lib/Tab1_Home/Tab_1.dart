b05a91f5b8a2e390916c9558b23acaf6cfd5ccad
Nam Jin <jinoj@naver.com>
2023-11-26 06:55:58 +0900

    design : tab1(add appbar)

--- lib/Tab1_Home/Tab_1.dart
+++ lib/Tab1_Home/Tab_1.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:photois/Main/data.dart';
 
+import '../Tab3_Add/add_post.dart';
+
 class Tab1 extends StatefulWidget {
   const Tab1({super.key});
 
@@ -16,122 +18,121 @@ class _Tab1State extends State<Tab1> {
     const String imageLogoName = 'assets/images/PHOTOIS_LOGO.png';
 
     return Scaffold(
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          SizedBox(
-            height: sizeController.screenHeight * 0.05,
-          ),
-          Padding(
-            padding: EdgeInsets.all(sizeController.screenHeight.value * 0.02),
-            child: Image.asset(
-              imageLogoName,
-              width: sizeController.screenWidth.value * 0.3,
-            ),
-          ),
-          const Divider(
-              thickness: 2, indent: 0, endIndent: 0, color: Colors.black),
-          Expanded(
-            child: SingleChildScrollView(
-              child: Padding(
-                padding:
-                    EdgeInsets.all(sizeController.screenHeight.value * 0.02),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      " 주변 인기사진 스팟",
-                      style: TextStyle(
-                          fontSize: sizeController.mainFontSize.value),
-                    ),
-                    SizedBox(
-                      height: sizeController.screenWidth.value * 0.3,
-                      child: ListView(
-                        scrollDirection: Axis.horizontal,
-                        children: [
-                          Container(
-                            width: sizeController.screenWidth.value * 0.3,
-                            color: Colors.deepOrangeAccent,
-                          ),
-                          SizedBox(
-                            width: sizeController.screenWidth.value * 0.02,
-                          ),
-                          Container(
-                            width: sizeController.screenWidth.value * 0.3,
-                            color: Colors.deepOrangeAccent,
-                          ),
-                          SizedBox(
-                            width: sizeController.screenWidth.value * 0.02,
-                          ),
-                          Container(
-                            width: sizeController.screenWidth.value * 0.3,
-                            color: Colors.deepOrangeAccent,
-                          ),
-                          SizedBox(
-                            width: sizeController.screenWidth.value * 0.02,
-                          ),
-                          Container(
-                            width: sizeController.screenWidth.value * 0.3,
-                            color: Colors.deepOrangeAccent,
-                          ),
-                          SizedBox(
-                            width: sizeController.screenWidth.value * 0.02,
-                          ),
-                          Container(
-                            width: sizeController.screenWidth.value * 0.3,
-                            color: Colors.deepOrangeAccent,
-                          ),
-                          SizedBox(
-                            width: sizeController.screenWidth.value * 0.02,
-                          ),
-                        ],
-                      ),
-                    ),
-                    SizedBox(
-                      height: sizeController.screenHeight.value * 0.03,
-                    ),
-                    Text(
-                      " 주변 인기사진 스팟",
-                      style: TextStyle(
-                          fontSize: sizeController.mainFontSize.value),
-                    ),
-                    Container(
-                      height: sizeController.screenWidth.value * 0.3,
-                      width: sizeController.screenWidth.value * 0.3,
-                      color: Colors.deepOrangeAccent,
-                    ),
-                    SizedBox(
-                      height: sizeController.screenHeight.value * 0.03,
-                    ),
-                    Text(
-                      " 주변 인기사진 스팟",
-                      style: TextStyle(
-                          fontSize: sizeController.mainFontSize.value),
-                    ),
-                    Container(
-                      height: sizeController.screenWidth.value * 0.3,
-                      width: sizeController.screenWidth.value * 0.3,
-                      color: Colors.deepOrangeAccent,
-                    ),
-                    SizedBox(
-                      height: sizeController.screenHeight.value * 0.03,
-                    ),
-                    Text(
-                      " 주변 인기사진 스팟",
-                      style: TextStyle(
-                          fontSize: sizeController.mainFontSize.value),
-                    ),
-                    Container(
-                      height: sizeController.screenWidth.value * 0.3,
-                      width: sizeController.screenWidth.value * 0.3,
-                      color: Colors.deepOrangeAccent,
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ),
+      appBar: AppBar(
+        title: Image.asset(
+          imageLogoName,
+          width: sizeController.screenWidth.value * 0.3,
+        ),
+        actions: [
+          IconButton(
+              onPressed: () {
+                Get.to(const Tab3());
+              },
+              icon: const Icon(Icons.add_photo_alternate_outlined))
         ],
+        bottom: const PreferredSize(
+          preferredSize: Size.fromHeight(-10),
+          child: Divider(
+            height: 2,
+            color: Colors.black,
+            thickness: 2, // 줄의 색상 설정
+          ),
+        ),
+      ),
+      body: Expanded(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.all(sizeController.screenHeight.value * 0.02),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  " 주변 인기사진 스팟",
+                  style: TextStyle(fontSize: sizeController.mainFontSize.value),
+                ),
+                SizedBox(
+                  height: sizeController.screenWidth.value * 0.3,
+                  child: ListView(
+                    scrollDirection: Axis.horizontal,
+                    children: [
+                      Container(
+                        width: sizeController.screenWidth.value * 0.3,
+                        color: Colors.deepOrangeAccent,
+                      ),
+                      SizedBox(
+                        width: sizeController.screenWidth.value * 0.02,
+                      ),
+                      Container(
+                        width: sizeController.screenWidth.value * 0.3,
+                        color: Colors.deepOrangeAccent,
+                      ),
+                      SizedBox(
+                        width: sizeController.screenWidth.value * 0.02,
+                      ),
+                      Container(
+                        width: sizeController.screenWidth.value * 0.3,
+                        color: Colors.deepOrangeAccent,
+                      ),
+                      SizedBox(
+                        width: sizeController.screenWidth.value * 0.02,
+                      ),
+                      Container(
+                        width: sizeController.screenWidth.value * 0.3,
+                        color: Colors.deepOrangeAccent,
+                      ),
+                      SizedBox(
+                        width: sizeController.screenWidth.value * 0.02,
+                      ),
+                      Container(
+                        width: sizeController.screenWidth.value * 0.3,
+                        color: Colors.deepOrangeAccent,
+                      ),
+                      SizedBox(
+                        width: sizeController.screenWidth.value * 0.02,
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: sizeController.screenHeight.value * 0.03,
+                ),
+                Text(
+                  " 주변 인기사진 스팟",
+                  style: TextStyle(fontSize: sizeController.mainFontSize.value),
+                ),
+                Container(
+                  height: sizeController.screenWidth.value * 0.3,
+                  width: sizeController.screenWidth.value * 0.3,
+                  color: Colors.deepOrangeAccent,
+                ),
+                SizedBox(
+                  height: sizeController.screenHeight.value * 0.03,
+                ),
+                Text(
+                  " 주변 인기사진 스팟",
+                  style: TextStyle(fontSize: sizeController.mainFontSize.value),
+                ),
+                Container(
+                  height: sizeController.screenWidth.value * 0.3,
+                  width: sizeController.screenWidth.value * 0.3,
+                  color: Colors.deepOrangeAccent,
+                ),
+                SizedBox(
+                  height: sizeController.screenHeight.value * 0.03,
+                ),
+                Text(
+                  " 주변 인기사진 스팟",
+                  style: TextStyle(fontSize: sizeController.mainFontSize.value),
+                ),
+                Container(
+                  height: sizeController.screenWidth.value * 0.3,
+                  width: sizeController.screenWidth.value * 0.3,
+                  color: Colors.deepOrangeAccent,
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
